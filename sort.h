68137a9f466676ef7c1aa95028830c2fce20ba49
root <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>
2022-08-16 19:36:21 -0700

    added header and print_array

---
+++ sort.h
@@ -0,0 +1,28 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * struct listint_s - Doubly linked lisy node
+ * @n: interger stored in the node
+ * @prev: pointer to the prev element
+ * @next: pointer to the next element
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
